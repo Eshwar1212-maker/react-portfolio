ae295c2483fe8a715065c6ab9983e4069ca2efaa
Eshwar Tangirala <eshwartangirala11@gmail.com>
2023-06-01 18:58:01 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Hero from "./components/Hero"
+import Nav from "./components/Nav"
+
+function App() {
+
+  return (
+   <div className="text-white font-poppins pb-12">
+    <Nav />
+    <Hero />
+   </div>
+  )
+}
+
+export default App
